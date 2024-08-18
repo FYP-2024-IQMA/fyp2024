abc9b14d27042bb3c23834e16944f9eb5a5a9367
Germaine <tanlixuangermaine@gmail.com>
2024-08-19 01:49:37 +0800

    SCRUM-38 chatHistory CRD endpoint

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -6,12 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const accountsRouter_1 = __importDefault(require("./routes/accountsRouter"));
+const chatRouter_1 = __importDefault(require("./routes/chatRouter"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 const port = 3000;
 app.use(express_1.default.json());
-// app.use(accountsRouter);
 app.use('/accounts', accountsRouter_1.default);
+app.use("/chat", chatRouter_1.default);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import accountsRouter from './routes/accountsRouter';
+import chatRouter from './routes/chatRouter';
 
 const app = express();
 app.use(cors());
@@ -8,8 +9,8 @@ const port = 3000;
 
 app.use(express.json());
 
-// app.use(accountsRouter);
 app.use('/accounts', accountsRouter);
+app.use("/chat", chatRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
