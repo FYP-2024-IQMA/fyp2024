f9fafcc33f976250ddbe59dd74cef2633f6efdd5
rrachea <rachel.sng.2021@scis.smu.edu.sg>
2024-08-28 00:39:17 +0800

    update yaml

--- frontend/iQMA-Skills-Builder/app/index.tsx
+++ frontend/iQMA-Skills-Builder/app/index.tsx
@@ -1,30 +1,30 @@
-import { Text, View, Image, ActivityIndicator } from "react-native";
+import { ActivityIndicator, Image, Text, View } from "react-native";
+
+import { AuthContext } from "@/context/AuthContext";
 import { LoginButton } from "@/components/LoginButton";
 import { LogoVisual } from "@/components/LogoVisual";
-import { Profile } from "@/components/Profile";
 import { LogoutButton } from "@/components/LogoutButton";
+import { Profile } from "@/components/Profile";
 import { useContext } from "react";
-import { AuthContext } from "@/context/AuthContext";
 
-// where things show up
+// where things show upp
 export default function Index() {
+  const { currentUser, isLoading } = useContext(AuthContext);
 
-    const { currentUser, isLoading } = useContext(AuthContext);
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#C3B1FF",
+      }}
+    >
+      <LogoVisual></LogoVisual>
+      <LoginButton></LoginButton>
 
-    return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: '#C3B1FF'
-            }}
-        >
-          <LogoVisual></LogoVisual>
-            <LoginButton></LoginButton>
-            
-            {/* For Testing & Debugging */}
-            <Profile></Profile> 
-        </View>
-    );
+      {/* For Testing & Debugging */}
+      <Profile></Profile>
+    </View>
+  );
 }
