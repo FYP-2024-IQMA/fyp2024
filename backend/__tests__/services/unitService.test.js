5c5fdea1e72e5a49d8aeff28d138ba229bbebb14
Fadhli <marvelouslyfadhli@gmail.com>
2024-09-06 14:28:16 +0800

    SCRUM-117 Add Test Cases for Unit Service

--- backend/__tests__/services/unitService.test.js
+++ backend/__tests__/services/unitService.test.js
@@ -16,7 +16,6 @@ afterEach(() => {
     consoleErrorSpy.mockRestore();
 });
 
-
 describe("getNoOfUnitPerSection", () => {
     const mockCount = 2;
     const expectedResult = 2;
@@ -36,14 +35,122 @@ describe("getNoOfUnitPerSection", () => {
     it("should throw an error when there is an error from supabase", async () => {
         const errorMessage = "Failed to fetch total no. of unit per section";
 
-        const mockEq = jest
-            .fn()
-            .mockResolvedValue({ count: mockCount, error: new Error(errorMessage) });
+        const mockEq = jest.fn().mockResolvedValue({
+            count: mockCount,
+            error: new Error(errorMessage),
+        });
         const mockSelect = jest.fn().mockReturnValue({ eq: mockEq });
         supabase.from.mockReturnValue({ select: mockSelect });
 
-        await expect(unitService.getNoOfUnitPerSection("SEC0000")).rejects.toThrow(errorMessage);
+        await expect(
+            unitService.getNoOfUnitPerSection("SEC0000")
+        ).rejects.toThrow(errorMessage);
         expect(consoleErrorSpy).toHaveBeenCalledWith(new Error(errorMessage));
     });
 });
 
+describe("getAllUnitsBySection", () => {
+    const expectedResult = [
+        {
+            sectionID: "SEC0001",
+            unitID: "UNIT0001",
+            unitName: "Foundations of Communication",
+            unitDescription:
+                "🎉 Get ready to dive into the exciting world of communication!\r\n📚 Let's decode the mysteries of verbal and non-verbal signals, uncover the secrets of various communication styles, and embark on a journey through behavioral insights concepts to unlock the power of effective communication! 🚀",
+            assessmentIntro:
+                "🌟 Ready to level up your communication game? Dive into our ultimate cheat sheet packed with tips and tricks to decode the secret language of verbal and non-verbal signals!\r\n📚 From mastering tone and body language to recognizing diverse communication styles and behavioral insights, this guide is your passport to engaging and effective interactions.\r\nWhether you're looking to fine-tune your speaking style or understand others better, we've got you covered with essential insights and fun pointers. 🎯",
+            realityCheck:
+                "🎉 Welcome to the ultimate party mixer challenge!\r\n🎊 Get ready to navigate diverse conversations, decode communication styles, and master the art of making meaningful connections! 🚀",
+            scenario:
+                "Imagine you're attending a bustling networking event filled with professionals from various industries. As you navigate through the crowd, you notice people engaged in conversations, exchanging business cards, and forming connections.\r\nSuddenly, you spot a potential client across the room who seems disinterested in the conversation they're having. How do you approach them and effectively convey your message to capture their attention and leave a lasting impression?",
+            dateCreated: "2024-08-18T14:59:01.549137+00:00",
+        },
+    ];
+
+    it("returns all units of section", async () => {
+        const mockEq = jest
+            .fn()
+            .mockResolvedValue({ data: expectedResult, error: null });
+        const mockSelect = jest.fn().mockReturnValue({ eq: mockEq });
+        supabase.from.mockReturnValue({ select: mockSelect });
+
+        const result = await unitService.getAllUnitsBySection(
+            expectedResult[0].sectionID
+        );
+
+        expect(result).toEqual(expectedResult);
+    });
+
+    it("should throw an error when there is an error from supabase", async () => {
+        const errorMessage = "Failed to fetch all units of Section";
+
+        const mockEq = jest.fn().mockResolvedValue({
+            data: expectedResult,
+            error: new Error(errorMessage),
+        });
+        const mockSelect = jest.fn().mockReturnValue({ eq: mockEq });
+        supabase.from.mockReturnValue({ select: mockSelect });
+
+        await expect(
+            unitService.getAllUnitsBySection("SEC0056")
+        ).rejects.toThrow(errorMessage);
+        expect(consoleErrorSpy).toHaveBeenCalledWith(new Error(errorMessage));
+    });
+});
+
+describe("getUnitDetailsBySectionAndUnit", () => {
+    const sectionID = "SEC0001";
+    const unitID = "UNIT0001";
+
+    const expectedResult = {
+        sectionID: "SEC0001",
+        unitID: "UNIT0001",
+        unitName: "Foundations of Communication",
+        unitDescription:
+            "🎉 Get ready to dive into the exciting world of communication!\r\n📚 Let's decode the mysteries of verbal and non-verbal signals, uncover the secrets of various communication styles, and embark on a journey through behavioral insights concepts to unlock the power of effective communication! 🚀",
+        assessmentIntro:
+            "🌟 Ready to level up your communication game? Dive into our ultimate cheat sheet packed with tips and tricks to decode the secret language of verbal and non-verbal signals!\r\n📚 From mastering tone and body language to recognizing diverse communication styles and behavioral insights, this guide is your passport to engaging and effective interactions.\r\nWhether you're looking to fine-tune your speaking style or understand others better, we've got you covered with essential insights and fun pointers. 🎯",
+        realityCheck:
+            "🎉 Welcome to the ultimate party mixer challenge!\r\n🎊 Get ready to navigate diverse conversations, decode communication styles, and master the art of making meaningful connections! 🚀",
+        scenario:
+            "Imagine you're attending a bustling networking event filled with professionals from various industries. As you navigate through the crowd, you notice people engaged in conversations, exchanging business cards, and forming connections.\r\nSuddenly, you spot a potential client across the room who seems disinterested in the conversation they're having. How do you approach them and effectively convey your message to capture their attention and leave a lasting impression?",
+        dateCreated: "2024-08-18T14:59:01.549137+00:00",
+    };
+
+    it("returns unit details of section and unit", async () => {
+        const mockSingle = jest
+            .fn()
+            .mockResolvedValue({ data: expectedResult, error: null });
+
+        const mockEq2 = jest.fn().mockReturnValue({ single: mockSingle });
+        const mockEq1 = jest.fn().mockReturnValue({ eq: mockEq2 });
+        const mockSelect = jest.fn().mockReturnValue({ eq: mockEq1 });
+
+        supabase.from.mockReturnValue({ select: mockSelect });
+
+        const result = await unitService.getUnitDetailsBySectionAndUnit({
+            sectionID,
+            unitID,
+        });
+        expect(result).toEqual(expectedResult);
+    });
+
+    it("should throw an error when there is an error from supabase", async () => {
+        const errorMessage = "Failed to fetch unit details of Section and Unit";
+
+        const mockSingle = jest.fn().mockResolvedValue({
+            data: expectedResult,
+            error: new Error(errorMessage),
+        });
+        const mockEq2 = jest.fn().mockReturnValue({ single: mockSingle });
+        const mockEq1 = jest.fn().mockReturnValue({ eq: mockEq2 });
+        const mockSelect = jest.fn().mockReturnValue({ eq: mockEq1 });
+
+        supabase.from.mockReturnValue({ select: mockSelect });
+
+        await expect(
+            unitService.getUnitDetailsBySectionAndUnit("SEC0056")
+        ).rejects.toThrow(errorMessage);
+        expect(consoleErrorSpy).toHaveBeenCalledWith(new Error(errorMessage));
+    });
+});
