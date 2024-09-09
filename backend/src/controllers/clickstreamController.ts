88c95945d787729cb37f10690dc739b430861625
Xuanli <xuanli743@gmail.com>
2024-09-09 15:46:05 +0800

    SCRUM-110 edit message for status 200

--- backend/src/controllers/clickstreamController.ts
+++ backend/src/controllers/clickstreamController.ts
@@ -6,7 +6,7 @@ export const sendMessage = async (req: Request, res: Response) => {
 
     try {
         const message = await clickstreamService.sendMessage(messageBody);
-        res.status(200).json(message);
+        res.status(200).json({ message: 'Published message successfully' });
     } catch (e) {
         res.status(500).json({ error: "Failed to send data" , e});
     }
