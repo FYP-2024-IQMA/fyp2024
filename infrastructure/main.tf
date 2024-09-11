48dffac302fea1978c47f4773804f80124e8f7e5
Tan Yi Peng <tanyipeng834@gmail.com>
2024-09-11 09:25:42 +0800

    finished monitoring and alerting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -262,6 +262,7 @@ resource "aws_instance" "app_instance_1" {
   key_name      = "server1-key"                # Replace with your existing key pair name
   subnet_id     = aws_subnet.private_subnet_1.id
   private_ip = "10.0.188.247"
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
 
   vpc_security_group_ids = [aws_security_group.app_instance_sg.id]
 
@@ -276,6 +277,7 @@ resource "aws_instance" "app_instance_2" {
   key_name      = "server2-key"                # Replace with your existing key pair name
   subnet_id     = aws_subnet.private_subnet_2.id
   private_ip = "10.0.237.165"
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
 
   vpc_security_group_ids = [aws_security_group.app_instance_sg.id]
 
@@ -415,14 +417,14 @@ output "loadbalancer_public_ip"{
 
 
 # S3 bucket to store athena output data
-resource "aws_s3_bucket" "athena_output" {
-  bucket = "isb-raw-data-athena-output"
+# resource "aws_s3_bucket" "athena_output" {
+#   bucket = "isb-raw-data-athena-output"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
 
 # Athena Workgroup
 resource "aws_athena_workgroup" "athena_workgroup" {
@@ -491,3 +493,59 @@ resource "aws_glue_catalog_table" "athena_table" {
 
   depends_on = [aws_athena_database.athena_db]
 }
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_role_policy" "ec2_role_policy" {
+  name = "ec2-role-policy"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_cloudwatch_log_group" "my_log_group" {
+  name = "iqma-log-group"
+}
+
+
+
