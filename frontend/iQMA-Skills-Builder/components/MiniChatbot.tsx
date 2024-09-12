5e1ed4131a40dd1aa354bff835e2fc782cb2d506
rrachea <rachel.sng.2021@scis.smu.edu.sg>
2024-09-12 13:04:26 +0800

    expand the content in minichatbot

--- frontend/iQMA-Skills-Builder/components/MiniChatbot.tsx
+++ frontend/iQMA-Skills-Builder/components/MiniChatbot.tsx
@@ -1,13 +1,17 @@
 import {
     Alert,
+    Keyboard,
+    KeyboardAvoidingView,
+    Platform,
     ScrollView,
     StyleSheet,
     Text,
     TextInput,
     TouchableOpacity,
+    TouchableWithoutFeedback,
     View,
 } from 'react-native';
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 
 import {AntDesign} from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -20,68 +24,82 @@ import {Feather} from '@expo/vector-icons';
 import {useDrawerStatus} from '@react-navigation/drawer';
 import {useNavigation} from '@react-navigation/native';
 
-// import {TextInput} from 'react-native-gesture-handler';
-
 // Getting response from chatbot -> to add examples and summarise
-// const getChatbotResponse = async (
-//     role: string,
-//     message: string,
-//     history?: Array<{role: string; content: string}>
-// ) => {
-//     try {
-//         const response = await fetch(
-//             `http://${process.env.EXPO_PUBLIC_LOCALHOST_URL}:8000/generate`,
-//             {
-//                 method: 'POST',
-//                 headers: {
-//                     'Content-Type': 'application/json',
-//                 },
-//                 body: JSON.stringify({
-//                     role: role,
-//                     content: message,
-//                     ...(history && {history}),
-//                 }),
-//             }
-//         );
-//         const data = await response.json();
-//         return data;
-//     } catch (error) {
-//         console.error('Error while getting chatbot response:', error);
-//     }
-// };
+const getChatbotResponse = async (
+    role: string,
+    message: string,
+    history?: Array<{role: string; content: string}>
+) => {
+    try {
+        // const response = await fetch(`http://10.0.2.2:8000/generate`, {
+
+        const response = await fetch(`http://10.0.2.2:8000/generate`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                role: role,
+                content: message,
+                ...(history && {history}),
+            }),
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error while getting chatbot response:', error);
+    }
+};
 
 // Save chat history
-// const saveChatHistory = async (
-//     userId: string,
-//     sectionId: string,
-//     queryPair: {role: string; content: string}[]
-// ) => {
-//     try {
-//         const body = {
-//             userID: userId,
-//             sectionID: sectionId,
-//             queryPair: queryPair,
-//         };
+const saveChatHistory = async (
+    userId: string,
+    sectionId: string,
+    queryPair: {role: string; content: string}[]
+) => {
+    try {
+        const body = {
+            userID: userId,
+            sectionID: sectionId,
+            queryPair: queryPair,
+        };
 
-//         console.log(body);
-//         console.log('inside savechathistory function in mini');
-//         const url = `${process.env.EXPO_PUBLIC_BACKEND_URL}/chat/createchathistory`;
+        console.log(body);
+        console.log('inside savechathistory function in mini');
+        const url = `${process.env.EXPO_PUBLIC_BACKEND_URL}/chat/createchathistory`;
 
-//         const response = await fetch(url, {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify(body),
-//         });
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        });
 
-//         const data = await response.json();
+        const data = await response.json();
 
-//         console.log('Status: ', data.status);
-//     } catch (error) {
-//         console.error('Error while saving chat history:', error);
-//     }
-// };
+        console.log('Status: ', data.status);
+    } catch (error) {
+        console.error('Error while saving chat history:', error);
+    }
+};
+
+//get reflection qn from backend
+const reflectionQuestion = async (
+    sectionID: string,
+    unitID: string,
+    lessonID: string
+) => {
+    try {
+        const url = `${process.env.EXPO_PUBLIC_BACKEND_URL}/getquestions/${sectionID}/${unitID}/${lessonID}}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log('refkectuib qb', data);
+        return data;
+    } catch (error) {
+        console.error('Error while getting reflection question:', error);
+    }
+};
 
 const MiniChatbot: React.FC = () => {
     const [message, setMessage] = useState('');
@@ -89,7 +107,11 @@ const MiniChatbot: React.FC = () => {
         []
     );
     const {currentUser, isLoading} = useContext(AuthContext);
+    const scrollViewRef = useRef<ScrollView>(null);
+
     const sectionID = 'SEC0001';
+    const lessonID = '4c';
+    const unitID = 'UNI0001';
 
     // handle user input
     const handleSend = async (message: string) => {
@@ -98,8 +120,6 @@ const MiniChatbot: React.FC = () => {
         const userMessage = {text: message, isUser: true};
         console.log(userMessage);
         const newMessages = [...messages, userMessage];
-        // console.log('newmessages');
-        // console.log(newMessages);
         setMessages(newMessages);
         setMessage('');
 
@@ -110,11 +130,8 @@ const MiniChatbot: React.FC = () => {
         }));
         console.log(history);
 
-        // const response = await getChatbotResponse('user', message, history);
-        const response = {
-            role: 'system',
-            content: 'hello i am cookie brave',
-        };
+        const response = await getChatbotResponse('user', message, history);
+
         if (response) {
             // Add the chatbot response to the chat
             const botReply = {text: response.content, isUser: false};
@@ -127,14 +144,36 @@ const MiniChatbot: React.FC = () => {
                 {role: 'assistant', content: response.content},
             ];
 
+            scrollViewRef.current?.scrollToEnd({animated: true});
+
             // saveChatHistory(currentUser.sub, sectionID, queryPair);
         }
     };
+
+    useEffect(() => {
+        if (sectionID && lessonID && unitID) {
+            async () => {
+                const response = await reflectionQuestion(
+                    sectionID,
+                    unitID,
+                    lessonID
+                );
+                console.log(response);
+                setMessages([response]);
+            };
+        }
+    }, [sectionID, lessonID, unitID]);
     return (
         <>
             <View style={styles.container}>
                 <View style={styles.purpleBox}>
-                    <ScrollView contentContainerStyle={styles.chatContainer}>
+                    <ScrollView
+                        style={styles.scrollView}
+                        contentContainerStyle={styles.chatContainer}
+                        onContentSizeChange={() =>
+                            scrollViewRef.current?.scrollToEnd({animated: true})
+                        }
+                    >
                         {messages.map((msg, index) => (
                             <ChatBubble
                                 key={index}
@@ -170,18 +209,22 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFFFFF',
     },
     chatContainer: {
-        padding: 10,
-        margin: 5,
-        // width: 600,
+        flexGrow: 1,
+        width: '100%',
+    },
+    scrollView: {
+        flex: 1,
+        width: '100%',
     },
     purpleBox: {
         height: 450,
         borderWidth: 1,
         borderColor: '#C3B1FF',
-        width: '95%',
+        width: '98%',
         padding: 15,
         borderRadius: 10,
         alignItems: 'center',
+        justifyContent: 'space-between',
     },
     sendButtonCircle: {
         width: 40,
