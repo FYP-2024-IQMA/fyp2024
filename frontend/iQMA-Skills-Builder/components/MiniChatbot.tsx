356c416780965305dba84cb5911b9f8bedccae31
rrachea <rachel.sng.2021@scis.smu.edu.sg>
2024-09-19 16:21:28 +0800

    set up rabbitmq and log to record chat response

--- frontend/iQMA-Skills-Builder/components/MiniChatbot.tsx
+++ frontend/iQMA-Skills-Builder/components/MiniChatbot.tsx
@@ -1,17 +1,7 @@
 import * as chatInputFunctions from '@/components/ChatInput';
 
-import {
-    Alert,
-    Keyboard,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    TouchableWithoutFeedback,
-    View,
-} from 'react-native';
 import React, {useContext, useEffect, useRef, useState} from 'react';
+import {ScrollView, StyleSheet, View} from 'react-native';
 
 import {AuthContext} from '@/context/AuthContext';
 import {ChatBubble} from '@/components/ChatBubble';
@@ -57,6 +47,7 @@ const MiniChatbot: React.FC<MiniChatbotProps> = ({onChatHistoryUpdate}) => {
     const {currentUser, isLoading} = useContext(AuthContext);
     const [reflectionQn, setReflectionQn] = useState<string>('');
     const scrollViewRef = useRef<ScrollView>(null);
+    const [responseTime, setResponseTime] = useState<number>(0);
 
     const sectionID = 'SEC0001';
     const unitID = 'UNIT0002';
@@ -121,6 +112,8 @@ const MiniChatbot: React.FC<MiniChatbotProps> = ({onChatHistoryUpdate}) => {
             content: msg.text,
         }));
 
+        const start = Date.now();
+
         const response = await chatInputFunctions.getChatbotResponse(
             'user',
             message,
@@ -128,6 +121,12 @@ const MiniChatbot: React.FC<MiniChatbotProps> = ({onChatHistoryUpdate}) => {
         );
 
         if (response) {
+            const end = Date.now();
+            const timeTaken = end - start;
+            setResponseTime(timeTaken);
+            console.log(`Time taken for chatbot to respond: ${timeTaken}ms`);
+            await sendToRabbitMQ(timeTaken);
+
             // Add the chatbot response to the chat
             const botReply = {text: response.content, isUser: false};
             const updatedMessages = [...newMessages, botReply];
@@ -147,6 +146,22 @@ const MiniChatbot: React.FC<MiniChatbotProps> = ({onChatHistoryUpdate}) => {
                 unitID,
                 queryPair
             );
+        }
+    };
+
+    const sendToRabbitMQ = async (timeTaken: number) => {
+        try {
+            await fetch(`${process.env.EXPO_PUBLIC_LOCALHOST_URL}/rabbitmq`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    timeTaken: timeTaken,
+                }),
+            });
+        } catch (error) {
+            console.error('Error sending time to RabbitMQ:', error);
         }
     };
 
